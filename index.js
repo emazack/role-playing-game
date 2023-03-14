ff720c3e34f8ae65809566b4ffeecbff4ead7c33
Zaccaria <emanuele.zaccaria@fabrick.com>
2023-03-14 17:28:27 +0100

    Add the getDiceHtml function

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ function getDiceRollArray(diceCount) {
     return newDiceRolls;
 }
 
+function getDiceHtml(diceCount){
+    return getDiceRollArray(diceCount).map(function(num){
+        return `<div class="dice">${num}</div>`
+    }).join('')
+}
+
+
 const hero = {
     elementId: "hero",
     name: "Wizard",
     avatar: "images/wizard.png",
     health: 60,
-    diceRoll: [3, 1, 4], 
     diceCount: 3
 }
 
@@ -20,23 +26,19 @@ const monster = {
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
-    diceRoll: [6],
     diceCount: 1
 }
 
 function renderCharacter(data) {
-    const { elementId, name, avatar, health, diceRoll, diceCount } = data;
-
-    const diceHtml = diceRoll.map(function(num){ 
-        return  `<div class="dice">${num}</div>`
-    }).join('')
+    const { elementId, name, avatar, health, diceCount } = data;
+    const diceHtml = getDiceHtml(diceCount)
 
     document.getElementById(elementId).innerHTML =
         `<div class="character-card">
             <h4 class="name"> ${name} </h4>
             <img class="avatar" src="${avatar}" />
             <div class="health">health: <b> ${health} </b></div>
-            <div class="dice-container">
+            <div class="dice-container">    
                 ${diceHtml}
             </div>
         </div>`;
