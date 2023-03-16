bf654b84e000951bf82a325c8c799f5f783d4326
Emanuele Zaccaria <em.zaccaria@gmail.com>
2023-03-16 14:21:55 +0100

    Separate getDiceRollArray in other file

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 import characterData from "./data";
-
-function getDiceRollArray(diceCount) {
-    return new Array(diceCount).fill(0).map(function () {
-        return Math.floor(Math.random() * 6) + 1
-    });
-}
+import getDiceRollArray from "./utils";
 
 function Character(data) {
     Object.assign(this, data)
