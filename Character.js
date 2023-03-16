857314fddb15e46224f7a55fb1a29810a01b126a
Emanuele Zaccaria <em.zaccaria@gmail.com>
2023-03-16 14:53:08 +0100

    Remove elementId

--- Character.js
+++ Character.js
@@ -10,7 +10,7 @@ function Character(data) {
     }
 
     this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;
+        const { name, avatar, health, diceCount } = this;
         let diceHtml = this.getDiceHtml(diceCount);
 
         return `
