32b116d6531b1fe958512fb7ff32f6c7e87447b5
Emanuele Zaccaria <em.zaccaria@gmail.com>
2023-03-16 13:32:24 +0100

    Separating concern about render and HTML creation

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 function getDiceRollArray(diceCount) {
-    return new Array(diceCount).fill(0).map(function () {
-        return Math.floor(Math.random() * 6) + 1
-    });
-}
-
-function getDiceHtml(diceCount) {
-    return getDiceRollArray(diceCount).map(function (num) {
-        return `<div class="dice">${num}</div>`
-    }).join('')
+    return new Array(diceCount).fill(0).map(function(){
+    return Math.floor(Math.random() * 6) + 1
+    });   
 }
 
 const hero = {
@@ -28,26 +22,32 @@ const monster = {
 
 function Character(data) {
     Object.assign(this, data)
-    this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;
-        const diceHtml = getDiceHtml(diceCount)
-
-        document.getElementById(elementId).innerHTML =
-            `<div class="character-card">
-            <h4 class="name"> ${name} </h4>
-            <img class="avatar" src="${avatar}" />
-            <div class="health">health: <b> ${health} </b></div>
-            <div class="dice-container">    
-                ${diceHtml}
-            </div>
-        </div>`;
-
+    
+     this.getDiceHtml = function(diceCount) {
+        return getDiceRollArray(diceCount).map(function(num){ 
+            return  `<div class="dice">${num}</div>`
+        }).join('')
     }
 
+    this.getCharacterHtml = function () {
+        const { elementId, name, avatar, health, diceCount } = this;      
+        let diceHtml = this.getDiceHtml(diceCount);
+        
+           return `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                <div class="dice-container">
+                    ${diceHtml}
+                </div>
+            </div>`;
+    }  
 }
 
 const wizard = new Character(hero)
+document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml()
+
 const orc = new Character(monster)
-wizard.getCharacterHtml()
-orc.getCharacterHtml()
+document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml()
 
