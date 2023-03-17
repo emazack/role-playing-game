ae201fb5c6e7e8022e91454562602d502a7d2014
Emanuele Zaccaria <em.zaccaria@gmail.com>
2023-03-17 18:30:30 +0100

    Add health bar

--- Character.js
+++ Character.js
@@ -1,43 +1,49 @@
 import { getDiceRollArray, getDicePlaceholderHtml } from './utils.js'
 
+const getPercentage = (remainingHealth, maximumHealth) => (100 * remainingHealth) / maximumHealth
+
 function Character(data) {
     Object.assign(this, data)
+    this.maxHealth = this.health
 
     this.diceArray = getDicePlaceholderHtml(this.diceCount)
-    
-/*
-CHALLENGE
-1. In the getDiceHtml method, there is an anonymous function 
-being used as an inline callback function. Replace it with an 
-arrow function.
-2. Do the same for the anonymous function in the takeDamage 
-method.
-3. Try to end up with the least amount of code possible!
-*/
 
     this.getDiceHtml = function () {
-        this.currentDiceScore = getDiceRollArray(this.diceCount);
-        this.diceArray = this.currentDiceScore.map((num)=> 
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
+        this.diceArray = this.currentDiceScore.map((num) =>
             `<div class="dice">${num}</div>`).join("")
     }
-    
-    this.takeDamage = function(attackScoreArray){  
+
+    this.takeDamage = function (attackScoreArray) {
         const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
         this.health -= totalAttackScore
-        if (this.health <= 0){
-            this.dead = true  
+        if (this.health <= 0) {
+            this.dead = true
             this.health = 0
-        } 
+        }
     }
     
-    this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;
+    this.getHealthBarHtml = function () {
+        const percent = getPercentage(this.health, this.maxHealth)
+        
+        return `
+        <div class="health-bar-outer">
+            <div class="health-bar-inner ${percent < 26 ? "danger" : ""} " 
+            style="width: ${percent}%;">
+            </div>
+        </div>`
+    }
+    
 
+    this.getCharacterHtml = function () {
+        const { elementId, name, avatar, health, diceCount } = this
+        const healthBar = this.getHealthBarHtml()
         return `
             <div class="character-card">
                 <h4 class="name"> ${name} </h4>
                 <img class="avatar" src="${avatar}" />
                 <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
                 <div class="dice-container">
                     ${this.diceArray}
                 </div>
