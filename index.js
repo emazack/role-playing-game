774e4b68a850c6f4e401187b32620e8e5ec4e3a2
Emanuele Zaccaria <em.zaccaria@gmail.com>
2023-03-17 16:57:35 +0100

    Set the end game

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ function attack() {
     wizard.takeDamage(orc.currentDiceScore)
     orc.takeDamage(wizard.currentDiceScore)
     render()
+    if(wizard.dead || orc.dead){
+        endGame()
+    }
+}
+
+function endGame(){
+    const endMessage = wizard.health === 0 && orc.health === 0 ?
+    "No victors - all creatures are dead" :
+    wizard.health > 0 ? "The Wizard Wins" :
+    "The Orc is Victorious"
 }
 
 document.getElementById("attack-button").addEventListener('click', attack)
